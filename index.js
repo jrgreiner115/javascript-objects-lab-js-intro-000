717d737c11a0f41ab026b0e7ad1d7366acc43b4d
Joe Greiner <jrgreiner115@gmail.com>
2018-02-13 15:55:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = new Object();
+var obj = {prop: 1};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, obj,{[key]:value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+ object[key] = value;
+ return object
+}
+function deleteFromObjectByKey(object, key) {
+var newObj = Object.assign({}, obj);
+delete newObj[key];
+return newObj;
+}
+
